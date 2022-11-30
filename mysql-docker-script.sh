3b1b0883550410cb6f9a80aa33846b4a3fdc8f6f
Fagner Moraes Carvalho <fagm.carvalho@gmail.com>
2022-11-30 09:48:25 -0300

    Update mysql-docker-script.sh

--- mysql-docker-script.sh
+++ mysql-docker-script.sh
@@ -1,1 +1,4 @@
 sudo docker run -e MYSQL_ROOT_PASSWORD=Senha123 --name mysql-A -d -p 3306:3306 mysql
+
+
+docker run -e MYSQL_ROOT_PASSWORD=Senha123 --name mysql-A -d -p 3306:3306 --volume=/data/mysql-A:/var/lib/mysql mysql
